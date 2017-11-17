3d63d48bda4f17db03f2d0fa4ccf2898baf62529
sasha <mycompanycomel@gmail.com>
2017-11-17 12:16:59 +0200

    fixed rspec

--- spec/model/task_spec.rb
+++ spec/model/task_spec.rb
@@ -17,14 +17,4 @@ describe Task, type: :model do
     expect(task.deadline).to eq date
   end
 
-  it 'increase_comments_qty check' do
-    task.increase_comments_qty
-    expect(task.comments_qty).to eq 1
-  end
-
-  it 'decrease_comments_qty check' do
-    task.comments_qty = 1
-    task.decrease_comments_qty
-    expect(task.comments_qty).to eq 0
-  end
 end
